0328c3b44c1adb8b847bd5a1cc3d5df5de6c6287
Choh Jing Yi <150232192+chohjingyia23cs0296@users.noreply.github.com>
2024-11-10 18:18:45 +0800

    Delete lab3/code/logout.php

--- lab3/code/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();//starting session
-session_unset();//unset the session
-session_destroy();//destroy the session
-header("Location:login.php");//redirect to the login page
-exit();//stop script
-?>
